ba41e87b921a107a7cb17cee4792baa0ff9005b0
Philipe Henrique Campos Barra <philipe.barra@basa.com.br>
2026-06-26 18:01:42 -0300

    Adiciona validação

--- src/main/java/med/vol/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/vol/api/medico/DadosCadastroMedico.java
@@ -1,12 +1,29 @@
 package med.vol.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.vol.api.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
+        @NotBlank
         String nome,
+
+        @NotBlank
+        @Email
         String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
         String crm,
+
+        @NotNull
         Especialidade especialidade,
+
+        @NotNull
+        @Valid//Informa que os elementos desta classe também possuem validações
         DadosEndereco endereco
 ) {
 
